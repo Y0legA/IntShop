dde9b73d4df7f3d6c277dffdf42493326606dee1
Oleg <myaddub@gmail.com>
2022-10-05 20:29:30 +0300

    init()

---
+++ InternetShopDZ/InternetShopDZ/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  InternetShopDZ
+//
+//  Created by Oleg_Yakovlev on 5.10.22.
+//
+
+import UIKit
+// Экран
+class ViewController: UIViewController {
+    // MARK: - Private IBoutlet
+    // MARK: - Private Visual Components
+    // MARK: - Public Properties
+    // MARK: - Private Properties
+    // MARK: - Initializers
+    // MARK: - LifeCycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    // MARK: - Public Methods
+    // MARK: - Private IBAction
+    // MARK: - Private Methods
+}
