2fbe04b5f65c3429c1d0c05e98cf6123798a5724
Oleg <myaddub@gmail.com>
2022-10-09 23:25:43 +0300

    Сделал 17 Урок

---
+++ InternetShopDZ/InternetShopDZ/Product.swift
@@ -0,0 +1,54 @@
+//
+//  Product.swift
+//  InternetShopDZ
+//
+//  Created by Oleg_Yakovlev on 9.10.22.
+//
+
+import UIKit
+
+// Класс товара
+final class Product: UIView {
+    // MARK: - Visual Components
+    private lazy var productImageView: UIImageView = {
+        let imageView = UIImageView(frame: CGRect(x: 25, y: 10, width: 70, height: 70))
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private lazy var nameProductLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.font = UIFont(name: "Avenir Next Demi Bold", size: 9)
+        label.numberOfLines = 0
+        label.textAlignment = .justified
+        label.frame = CGRect(x: 10, y: 93, width: 100, height: 37)
+        return label
+    }()
+    
+    // MARK: - Initializers
+    init(parameter: ProductInfo) {
+        super.init(frame: .zero)
+        self.productImageView.image = UIImage(named: parameter.productImageName)
+        self.nameProductLabel.text = parameter.productName
+        self.configureUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Public Methods
+    override func layoutSubviews() {
+           super.layoutSubviews()
+        backgroundColor =  #colorLiteral(red: 0.1107899025, green: 0.1054661646, blue: 0.1186901554, alpha: 1)
+           layer.cornerRadius = 10
+           frame.size = CGSize(width: 120, height: 150)
+       }
+    
+    // MARK: - Private Methods
+    private func configureUI() {
+        addSubview(productImageView)
+        addSubview(nameProductLabel)
+    }
+}
--- InternetShopDZ/InternetShopDZ/ViewControllers/SearchViewControllers/SearchViewController.swift
+++ InternetShopDZ/InternetShopDZ/ViewControllers/SearchViewControllers/SearchViewController.swift
@@ -7,29 +7,33 @@
 
 import UIKit
 
-// Экран поиска товара
+// Экран поиска
 final class SearchViewController: UIViewController {
     private enum Constants {
         static let search = "Поиск"
-        static let seed = "Недавно просмотренные"
-        static let clean = "Очистить"
-        static let requests = "Варианты запросов"
-        static let fontAvenir = "Avenir Next Demi Bold"
-        static let fontArial = "Arial"
-        static let placeholder = "Поиск по продуктам и магазинам"
-        static let goods = [("Чехол Incase Flat для MacBook Pro 16 дюймов", "Image"),
-                            ("Спортивный ремешок Black Unity для (к...", "4"),
-                            ("Чехол Incase Flat для MacBook Pro 16 дюймов", "2")
+        static let products = [("Чехол Incase Flat для MacBook Pro 16 дюймов", "case1"),
+                               ("Спортивный ремешок Black Unity для карманного", "clock1"),
+                               ("Кожаный чехол Incase Flat для MacBook Pro 16 дюймов, золото", "caseBrown1")
         ]
+        static let seed = "Недавно просмотренные"
+        static let searchIcon = "magnifyingglass"
+        static let clear = "Очистить"
+        static let fontAvenir = "Avenir Next"
+        static let fontAvenirDB = "Avenir Next Demi Bold"
+        static let fontArial = "Arial"
+        static let fontArialHebrew = "Arial Hebrew"
+        static let fontArialDB = "Arial Hebrew Bold"
+        static let requests = "Варианты запросов"
+        static let placeholder = "Поиск по продуктам и магазинам"
         static let textRequests = ["Air Pods", "Apple Care", "Beats", "Сравните модели iPhone"]
     }
     
-    // MARK: - Visual Components
+    // MARK: - Visual Properties
     private lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.text = Constants.search
         label.textColor = .label
-        label.font = UIFont(name: Constants.fontAvenir, size: 25)
+        label.font = UIFont(name: Constants.fontAvenirDB, size: 25)
         label.frame = CGRect(x: 0, y: view.frame.minY + 50, width: view.frame.width, height: 50)
         return label
     }()
@@ -45,7 +49,7 @@ final class SearchViewController: UIViewController {
         var label = UILabel()
         label.text = Constants.seed
         label.textColor = .label
-        label.font = UIFont(name: Constants.fontAvenir, size: 17)
+        label.font = UIFont(name: Constants.fontAvenirDB, size: 17)
         label.frame = CGRect(x: 0, y: productsSearchBar.frame.maxY + 35, width: view.frame.width / 3 * 2, height: 15)
         return label
     }()
@@ -57,26 +61,25 @@ final class SearchViewController: UIViewController {
                               width: view.frame.width / 3,
                               height: viewedLabel.frame.height)
         button.titleLabel?.font = UIFont(name: Constants.fontArial, size: 14) ?? UIFont()
-        button.setTitle(Constants.clean, for: .normal)
+        button.setTitle(Constants.clear, for: .normal)
         button.contentHorizontalAlignment = .right
         button.setTitleColor(UIColor.systemBlue, for: .normal)
         button.addTarget(self, action: #selector(clearSearchTextFieldAction), for: .touchDown)
         return button
     }()
     
-    private lazy var productOneView = makeView(0, clearButton.frame.maxY + 10.0,
-                                               Constants.goods[0].0, Constants.goods[0].1)
-    private lazy var productTwoView = makeView(143, clearButton.frame.maxY + 10.0,
-                                               Constants.goods[1].0, Constants.goods[1].1)
-    private lazy var productThreeView = makeView(286, clearButton.frame.maxY + 10.0,
-                                                 Constants.goods[2].0, Constants.goods[2].1)
+    private lazy var goodsScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.frame = CGRect(x: 0, y: clearButton.frame.maxY + 15, width: view.bounds.width, height: 150)
+        return scrollView
+    }()
     
     private lazy var requestsLabel: UILabel = {
         let label = UILabel()
         label.text = Constants.requests
         label.textColor = .label
-        label.font = UIFont(name: Constants.fontAvenir, size: 18)
-        label.frame = CGRect(x: 0, y: productOneView.frame.maxY + 20.0, width: view.frame.width, height: 50)
+        label.font = UIFont(name: Constants.fontAvenirDB, size: 18)
+        label.frame = CGRect(x: 0, y: goodsScrollView.frame.maxY + 20, width: view.frame.width, height: 50)
         return label
     }()
     
@@ -87,23 +90,24 @@ final class SearchViewController: UIViewController {
     }
     
     // MARK: - Private Actions
-    @objc private func handleTap(_ sender: UITapGestureRecognizer) {
-        let productViewController = SelectedProductViewController()
-        guard let index = sender.view?.tag else { return }
-        productViewController.productName = Constants.goods[index].0
-        productViewController.productImageName = Constants.goods[index].1
-        navigationController?.pushViewController(productViewController, animated: true)
+    @objc private func clearSearchTextFieldAction() {
+        productsSearchBar.text = String()
     }
     
-    @objc private func clearSearchTextFieldAction() {
-        productsSearchBar.text = ""
+    @objc private func handleTap(_ recognizer: UIGestureRecognizer) {
+        let selectVC = SelectedProductViewController()
+        let barButtonItem = UIBarButtonItem()
+        barButtonItem.title = Constants.search
+        navigationItem.backBarButtonItem = barButtonItem
+        selectVC.productName = Constants.products[recognizer.view?.tag ?? 0].0
+        navigationController?.pushViewController(selectVC, animated: true)
     }
     
     // MARK: - Private Methods
     private func configureUI() {
+        makeImagesForScrollView()
         configureView()
         makeRequestsLabel()
-        addRecognizer()
     }
     
     private func configureView() {
@@ -112,39 +116,32 @@ final class SearchViewController: UIViewController {
         view.addSubview(productsSearchBar)
         view.addSubview(viewedLabel)
         view.addSubview(clearButton)
-        view.addSubview(productOneView)
-        view.addSubview(productTwoView)
-        view.addSubview(productThreeView)
+        view.addSubview(goodsScrollView)
         view.addSubview(requestsLabel)
-        
     }
     
-    private func makeView(_ coordinateX: Double, _ coordinateY: Double, _ name: String, _ imageName: String) -> UIView {
-        let view = UIView()
-        view.frame = CGRect(x: coordinateX, y: coordinateY, width: 135, height: 180)
-        let imageView = UIImageView()
-        imageView.frame = CGRect(x: view.bounds.minX + 25, y: view.bounds.minY + 15, width: 80, height: 90)
-        imageView.contentMode = .scaleAspectFit
-        imageView.image = UIImage(named: imageName)
-        view.addSubview(imageView)
-        let label = UILabel()
-        label.text = name
-        label.textAlignment = .left
-        label.textColor = .label
-        label.font = UIFont(name: Constants.fontAvenir, size: 12)
-        label.numberOfLines = 0
-        label.textAlignment = .justified
-        label.frame = CGRect(x: view.bounds.minX + 10, y: view.bounds.midY + 20, width: 100, height: 50)
-        view.addSubview(label)
-        view.layer.cornerRadius = 15
-        view.backgroundColor = .systemGray6
-        return view
+    private func makeImagesForScrollView() {
+        var products: [Product] = []
+        Constants.products.forEach {
+            products.append(Product(parameter: ProductInfo(productName: $0.0, productImageName: $0.1)))
+        }
+        var xCoord = goodsScrollView.frame.minX
+        for (index, item) in products.enumerated() {
+            item.frame = CGRect(x: xCoord, y: 0, width: 140, height: 150)
+            item.tag = index
+            let recognizer = UITapGestureRecognizer()
+            recognizer.addTarget(self, action: #selector(handleTap))
+            item.addGestureRecognizer(recognizer)
+            goodsScrollView.addSubview(item)
+            xCoord += 135
+        }
+        goodsScrollView.contentSize = CGSize(width: xCoord, height: 150)
     }
     
     private func makeRequestsLabel() {
         var yCoordinate: CGFloat = requestsLabel.frame.maxY + 8
         for textRequest in Constants.textRequests {
-            let imageView = UIImageView(image: UIImage(systemName: "magnifyingglass"))
+            let imageView = UIImageView(image: UIImage(systemName: Constants.search))
             imageView.tintColor = .gray
             imageView.frame = CGRect(x: 0, y: yCoordinate, width: 13, height: 13)
             let label = UILabel(frame: CGRect(x: imageView.frame.maxX + 10,
@@ -164,17 +161,5 @@ final class SearchViewController: UIViewController {
             view.addSubview(label)
             yCoordinate += 42
         }
-    }
-    
-    private func addRecognizer() {
-        productOneView.tag = 0
-        productOneView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
-        productOneView.isUserInteractionEnabled = true
-        productTwoView.tag = 1
-        productTwoView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
-        productTwoView.isUserInteractionEnabled = true
-        productThreeView.tag = 2
-        productThreeView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
-        productThreeView.isUserInteractionEnabled = true
     }
 }
